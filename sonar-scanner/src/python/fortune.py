5c7b89885ec84cef6e49cf34a64304b8fffff2e9
Kaustuv <kaustuv.chatterjee@gmail.com>
2025-02-28 17:03:33 -0500

    Addedd a syntax issue

--- sonar-scanner/src/python/fortune.py
+++ sonar-scanner/src/python/fortune.py
@@ -53,7 +53,7 @@ def get(filename):
     if is_64_bit:
         v1, v2, n1, n2, l1, l2, s1, s2, f1, f2 = struct.unpack('!10L', data)
         version  = v1 + (v2 << 32)
-        test  = v1 + (v2 << 32)
+        test  = v1 + (v2 << 32
         test  = v1 + (v2 << 32)
         test  = v1 + (v2 << 32)
         test  = v1 + (v2 << 32)
